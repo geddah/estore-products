bde11298a0f26503764e16cf2d5b3ed42c54628e
anjalig <anjali.gosain91@gmail.com>
2021-06-10 12:01:26 -0700

    graphql implementation

---
+++ src/main/java/com/knockoff/estore/domain/Product.java
@@ -0,0 +1,71 @@
+package com.knockoff.estore.domain;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+@Builder
+
+public class Product {
+
+	String product_id;
+	String name;
+	String make;
+	String model;
+	String category;
+	int stock;
+	String description;
+	double price;
+	
+	public String getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
