f2ba956d6fd2ec7b4472bf28712f0f779693c420
Harsha Gedda <geddaharsha@gmail.com>
2021-08-06 11:52:33 -0500

    add variant service

---
+++ estore-product-variant-service/src/main/java/com/knockoff/product/variant/EstoreProductVariantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.knockoff.product.variant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreProductVariantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoreProductVariantServiceApplication.class, args);
+    }
+
+}
---
+++ estore-product-variant-service/src/test/java/com/knockoff/product/variant/EstoreProductVariantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knockoff.product.variant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreProductVariantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
